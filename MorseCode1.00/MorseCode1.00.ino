6228b5fb685d9c966049b2ecaa2045de8f19e01f
oddgas <oddgas@stud.ntnu.no>
2017-09-04 13:46:17 +0200

    Arduino is now functional

--- MorseCode1.00/MorseCode1.00.ino
+++ MorseCode1.00/MorseCode1.00.ino
@@ -7,6 +7,20 @@ int dotPin = 13;
 int dashPin = 7;
 int secPin = 5;
 int current;   
+long pauseTime=0;
+long buttonRelease=0;
+
+
+const float T = 200; // T is in ms. Change to prefered amount.
+const float dotTime = T;  
+const float dashTime = 3*T;
+const float letterPauseTime = 3*T;
+const float wordPauseTime = 7*T;
+
+const int dotSignal = 0; // Given by assignment.
+const int dashSignal = 1;
+const int letterPauseSignal = 2;
+const int wordPauseSignal = 3;
 
 void setup() {
   // initialize the button pin as a input:
@@ -22,37 +36,55 @@ void setup() {
 void loop()
 {
   prev_millis=millis(); // Counter for normal time
-
+  
   // While button is pressed we count
   while(digitalRead(buttonPin) == HIGH){
+    if (pauseTime != 0){
+      getPause(pauseTime); // Method implemented below
+      pauseTime=0;
+    }
     millis_pressed=millis()-prev_millis;
     digitalWrite(secPin, HIGH);
+    buttonRelease=millis();
+
   }
   
+  pauseTime=millis()-buttonRelease; // Time button is not pressed
   if(millis_pressed>50){    // Bigger than 50ms because of debounce
-     if (millis_pressed>=100 && millis_pressed <=200) {
-        digitalWrite(dotPin,HIGH);
-        delay(300);
-        // Python parse dot sign as "0".
-        Serial.println("0");
-        //Serial.print("Milliseconds held: ");
-        //Serial.println(millis_pressed);
+    if(millis_pressed>dotTime+100){
+      digitalWrite(dashPin,HIGH);
+      delay(300);
+      // Python parse dash as "1".
+      Serial.println(dashSignal);
+      //Serial.print("Milliseconds held: ");
+      //Serial.println(millis_pressed);
     }
-    if(millis_pressed>200 && millis_pressed <=400){
-        digitalWrite(dashPin,HIGH);
-        delay(300);
-        // Python parse dash as "1".
-        Serial.println("1");
-        //Serial.print("Milliseconds held: ");
-        //Serial.println(millis_pressed);
+    else{
+      digitalWrite(dotPin,HIGH);
+      delay(300);
+      // Python parse dot sign as "0".
+      Serial.println(dotSignal);
+      //Serial.print("Milliseconds held: ");
+      //Serial.println(millis_pressed);
     }
+
     
     millis_pressed=0;
     
   }
+  
   digitalWrite(dotPin,LOW);
   digitalWrite(dashPin,LOW);
   digitalWrite(secPin,LOW);
+}
+void getPause(long pauseTime){
+    if(pauseTime<letterPauseTime+200){
+      Serial.println(letterPauseSignal);
+    }
+    else{
+      Serial.println(wordPauseSignal);
+    }
+    // Make else method for resetting the process
 }
 
 
