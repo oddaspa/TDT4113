23f9dd2017935ec0d790c0107e857d62008719c1
oddgas <oddgas@stud.ntnu.no>
2017-09-05 13:47:33 +0200

    removed unnecessary code from arduino

--- Øving 1/MorseCode1.01/MorseCode1.01.ino
+++ Øving 1/MorseCode1.01/MorseCode1.01.ino
@@ -1,15 +1,15 @@
-int currState = HIGH;
-int prevState = HIGH;
 long prev_millis;
 long millis_pressed;
 int buttonPin = 3;  // the pin number for input (for me a push button)
 int dotPin = 13; 
 int dashPin = 7;
-int secPin = 5;
-int current;   
+
+int current;
+
 long pauseTime=0;
 long buttonRelease=0;
 
+// -----------------------   Given by assignment   ------------------------------//
 
 const float T = 200; // T is in ms. Change to prefered amount.
 const float dotTime = T;  
@@ -22,6 +22,8 @@ const int dashSignal = 2;
 const int letterPauseSignal = 3;
 const int wordPauseSignal = 4;
 
+//-------------------------------------------------------------------------------//
+
 void setup() {
   // initialize the button pin as a input:
   pinMode(buttonPin, INPUT);
@@ -29,8 +31,6 @@ void setup() {
   pinMode(dotPin, OUTPUT);
   // initialize the dashLED(Red) as output:
   pinMode(dashPin, OUTPUT);
-  // initialize the secLED(Blue) as output:
-  pinMode(secPin, OUTPUT);
   // initialize serial communication:
   Serial.begin(9600);
   
@@ -41,7 +41,6 @@ void loop()
   
   // While button is pressed we count
   while(digitalRead(buttonPin) == HIGH){
-    digitalWrite(secPin, HIGH);
     if (pauseTime != 0){
       getPause(pauseTime); // Method implemented below
       pauseTime=0;
@@ -56,28 +55,21 @@ void loop()
     if(millis_pressed>dotTime+100){
       digitalWrite(dashPin,HIGH);
       delay(300);
-      // Python parse dash as "1".
+      // Python parse dash as "2".
       Serial.print(dashSignal);
-      //Serial.print("Milliseconds held: ");
-      //Serial.println(millis_pressed);
     }
     else{
       digitalWrite(dotPin,HIGH);
       delay(300);
-      // Python parse dot sign as "0".
+      // Python parse dot sign as "1".
       Serial.print(dotSignal);
-      //Serial.print("Milliseconds held: ");
-      //Serial.println(millis_pressed);
     }
-
-    
+    // Reset millis_pressed after signal sent.
     millis_pressed=0;
     
   }
-  
   digitalWrite(dotPin,LOW);
   digitalWrite(dashPin,LOW);
-  digitalWrite(secPin,LOW);
 }
 void getPause(long pauseTime){
     if(pauseTime>letterPauseTime && pauseTime<letterPauseTime+400){
@@ -86,7 +78,6 @@ void getPause(long pauseTime){
     if(pauseTime>wordPauseTime+400){
       Serial.print(wordPauseSignal);
     }
-    // Make else method for resetting the process
 }
 
 
