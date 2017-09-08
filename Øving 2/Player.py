a851f5f9bcba1212096fe946b1c1d7c3e9c13723
oddgas <oddgas@stud.ntnu.no>
2017-09-08 19:07:23 +0200

    some updates

--- Øving 2/Player.py
+++ Øving 2/Player.py
@@ -3,7 +3,6 @@
 
 
 class player():
-
     #  This method chooses which action should be played and returns the value.
     def pick_action(self,action):
         if action >2 or action <0:
@@ -17,9 +16,18 @@ class player():
     # of each player. It can choose to learn from this information. (Especially if player is
     # historian or most_usual)
 
+    # sets name of player
+    def __set_name__(self, name):
+        self.name==name
 
+    # reports if it is a victory or loss
     def get_result(self):
-        player
 
+
+    # sends recent action back to
     def get_action(self):
         return self.action
+
+    def give_name(self):
+        return self.name
+
